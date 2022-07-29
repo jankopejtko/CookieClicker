33fd67eebd86d6d391d4737a8c3031ed25ddda24
jankopejtko <jan.kopejtko@email.cz>
2022-07-29 20:18:53 +0200

    upload code

---
+++ CookieClicker/CookieClicker/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace CookieClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(331, 168);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chocolate;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(15, 526);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cookies: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(207, 523);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Chocolate;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(15, 497);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(207, 494);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Chocolate;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.Location = new System.Drawing.Point(15, 468);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "auto spend all cookies";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Chocolate;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(740, 168);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Chocolate;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(740, 197);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Chocolate;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button6.Location = new System.Drawing.Point(740, 226);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(132, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(608, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(608, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 15.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(608, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ CookieClicker/CookieClicker/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace CookieClicker
+{
+    public partial class Form1 : Form
+    {
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer(Application.StartupPath + "\\theme.wav");
+        private Random rn = new Random();
+        private long CookiesCount = 0;
+        //building
+        private int numberOfBulding_1 = 0;
+        private int numberOfBulding_2 = 0;
+        private int numberOfBulding_3 = 0;
+        //money needed
+        private int moneyNeededForAutoClicker = 0;
+        private int moneyNeededForMultiplier = 0;
+        private int moneyNeededForBulding_1 = 0;
+        private int moneyNeededForBulding_2 = 0;
+        private int moneyNeededForBulding_3 = 0;
+        //bools canBuy
+        private bool canBuyAutoClicker = false;
+        private bool canBuyMultiplier = false;
+        private bool canBuyBuilding1 = false;
+        private bool canBuyBuilding2 = false;
+        private bool canBuyBuilding3 = false;
+        //bools enable
+        private bool AutoClicker = false;
+        private bool Multiplier = false;
+        private bool GCshown = false;
+        private bool exit = false;
+        //lvl
+        private int autoClickerLVL = 1;
+        private int MultiplierLVL = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+        public void init()
+        {
+            this.Text = "Cookie Clicker";
+            this.Icon = new Icon(Application.StartupPath + "\\PerfectCookie.ico");
+            this.BackgroundImage = Image.FromFile(Application.StartupPath + "\\background.png");
+            pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\PerfectCookie.png");
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new Size(900, 600);
+            this.MaximumSize = new Size(900, 600);
+            moneyNeededForAutoClicker = setNewPrice(20, label2, moneyNeededForAutoClicker);
+            moneyNeededForMultiplier = setNewPrice(100, label3, moneyNeededForMultiplier);
+            moneyNeededForBulding_1 = setNewPrice(50, label4, moneyNeededForBulding_1);
+            moneyNeededForBulding_2 = setNewPrice(500, label5, moneyNeededForBulding_2);
+            moneyNeededForBulding_3 = setNewPrice(2000, label6, moneyNeededForBulding_3);
+            player.PlayLooping();
+            button4.Text = "Buy building 1 num: " + numberOfBulding_1;
+            button5.Text = "Buy building 2 num: " + numberOfBulding_2;
+            button6.Text = "Buy building 3 num: " + numberOfBulding_3;
+        }
+        public void IsAchivment() 
+        {
+            if(CookiesCount >= 100000 && GCshown == false)
+            {
+                pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\GoldenCookie.png");
+                MessageBox.Show("Congratulation you have unlocked Golden Cookie");
+                GCshown = true;
+                return;
+            }
+            if (CookiesCount >= 10000000 && exit == false) 
+            {
+                MessageBox.Show("Congratulation you have no life");
+                Application.Exit();
+                exit = true;
+            }
+            else 
+            {
+                return;
+            }
+        }
+        public void loop()
+        {
+            button1.Text = "Buy Auto Clicker " + autoClickerLVL;
+            button2.Text = "Buy Cookie Multiplier " + MultiplierLVL;
+            canBuyAutoClicker = haveEnoughMoney(button1, canBuyAutoClicker, moneyNeededForAutoClicker);
+            canBuyMultiplier = haveEnoughMoney(button2, canBuyMultiplier, moneyNeededForMultiplier);
+            canBuyBuilding1 = haveEnoughMoney(button4, canBuyBuilding1, moneyNeededForBulding_1);
+            canBuyBuilding2 = haveEnoughMoney(button5, canBuyBuilding2, moneyNeededForBulding_2);
+            canBuyBuilding3 = haveEnoughMoney(button6, canBuyBuilding3, moneyNeededForBulding_3);
+            button4.Text = "Buy building 1 num: " + numberOfBulding_1;
+            button5.Text = "Buy building 2 num: " + numberOfBulding_2;
+            button6.Text = "Buy building 3 num: " + numberOfBulding_3;
+            autoClick();
+            label1.Text = "Cookies: " + CookiesCount.ToString();
+            IsAchivment();
+            GenerateGoldenPig();
+        }
+        private int setNewPrice(int price, Label label, int needed)
+        {
+            needed = price;
+            label.Text = "Price: " + needed;
+            return needed;
+        }
+        private bool haveEnoughMoney(Button button, bool b, int needed) 
+        {
+            if(CookiesCount < needed)
+            {
+                button.Cursor = Cursors.No;
+                button.Enabled = false;
+                b = false;
+            }
+            else
+            {
+                button.Cursor = Cursors.Hand;
+                button.Enabled = true;
+                b = true;
+            }
+            return b;
+        }
+        public void Shutdown() 
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void autoClick()
+        {
+            if (AutoClicker) 
+            {
+                CookiesCount = CookiesCount + (MultiplierLVL * autoClickerLVL);
+                timer1.Interval = 1000 / autoClickerLVL;
+            }
+        }
+        private void multiplier() 
+        {
+            if (Multiplier) 
+            {
+                CookiesCount = CookiesCount+1 * (MultiplierLVL + 1);
+            }
+            else 
+            {
+                CookiesCount = CookiesCount + 1;
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            multiplier();
+            label1.Text = "Cookies: " + CookiesCount.ToString();
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            loop();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (canBuyMultiplier)
+            {
+                CookiesCount = CookiesCount - moneyNeededForMultiplier;
+                moneyNeededForMultiplier = setNewPrice(moneyNeededForMultiplier * 15/10, label3, moneyNeededForMultiplier);
+                MultiplierLVL++;
+                Multiplier = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (canBuyAutoClicker)
+            {
+                CookiesCount = CookiesCount - moneyNeededForAutoClicker;
+                moneyNeededForAutoClicker = setNewPrice(moneyNeededForAutoClicker * 15/10, label2, moneyNeededForAutoClicker);
+                autoClickerLVL++;
+                AutoClicker = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a;
+            while (CookiesCount >= 0) 
+            {
+                canBuyAutoClicker = haveEnoughMoney(button1, canBuyAutoClicker, moneyNeededForAutoClicker);
+                canBuyMultiplier = haveEnoughMoney(button2, canBuyMultiplier, moneyNeededForMultiplier);
+                a = rn.Next(1, 3);
+                if (canBuyAutoClicker)
+                {
+                    CookiesCount = CookiesCount - moneyNeededForAutoClicker;
+                    moneyNeededForAutoClicker = setNewPrice(moneyNeededForAutoClicker * 15 / 10, label2, moneyNeededForAutoClicker);
+                    autoClickerLVL++;
+                    AutoClicker = true;
+                }
+                if (canBuyMultiplier)
+                {
+                    CookiesCount = CookiesCount - moneyNeededForMultiplier;
+                    moneyNeededForMultiplier = setNewPrice(moneyNeededForMultiplier * 15 / 10, label3, moneyNeededForMultiplier);
+                    MultiplierLVL++;
+                    Multiplier = true;
+                }
+                else 
+                {
+                    return;
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (numberOfBulding_1 > 0)
+            {
+                CookiesCount += 5 * numberOfBulding_1;
+            }
+            if (numberOfBulding_2 > 0)
+            {
+                CookiesCount += 25 * numberOfBulding_2;
+            }
+            if (numberOfBulding_3 > 0)
+            {
+                CookiesCount += 100 * numberOfBulding_3;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (canBuyBuilding1)
+            {
+                CookiesCount = CookiesCount - moneyNeededForBulding_1;
+                timer2.Enabled = true;
+                numberOfBulding_1++;
+                moneyNeededForBulding_1 = setNewPrice(moneyNeededForBulding_1 * 15 / 10, label4, moneyNeededForBulding_1);
+            }
+        }
+        private void GenerateGoldenPig() 
+        {
+            double rand = rn.Next(0, 100);
+
+            if (rand < 0.000002)
+            {
+                PictureBox pig = new PictureBox();
+                pig.Height = 20;
+                pig.Width = 20;
+                pig.Location = new Point(10,10);
+                pig.Image = Image.FromFile(Application.StartupPath + "\\goldenpig.png");
+            }
+            else 
+            {
+                return;
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (canBuyBuilding2)
+            {
+                CookiesCount = CookiesCount - moneyNeededForBulding_2;
+                timer2.Enabled = true;
+                numberOfBulding_2++;
+                moneyNeededForBulding_2 = setNewPrice(moneyNeededForBulding_2 * 15 / 10, label5, moneyNeededForBulding_2);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (canBuyBuilding3)
+            {
+                CookiesCount = CookiesCount - moneyNeededForBulding_3;
+                timer2.Enabled = true;
+                numberOfBulding_3++;
+                moneyNeededForBulding_3 = setNewPrice(moneyNeededForBulding_3 * 15 / 10, label6, moneyNeededForBulding_3);
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
